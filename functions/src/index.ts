dcb1838fe4a1bccd4096a795ddfd79b82eece761
Napatchol Thaipanich <patorseing@gmail.com>
2023-02-12 11:22:16 +0700

    chore: yml deploy only functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,16 +3,4 @@
 import * as functions from "firebase-functions";
 import {dialogflow} from "./dialogflow";
 
-// const runtimeOpts = {
-//   timeoutSeconds: 8,
-//   memory: "1GB" as const,
-//   minInstances: 1,
-// };
-// const region = "asia-northeast1";
-
-// exports.dialogflowFirebaseFulfillment = functions
-//     .region(region)
-//     .runWith(runtimeOpts)
-//     .https.onRequest(dialogflow);
-
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(dialogflow);
