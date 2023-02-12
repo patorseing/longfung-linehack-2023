306d46c7418b24f14bad0ae46109a59653880081
Napatchol Thaipanich <patorseing@gmail.com>
2023-02-12 11:03:43 +0700

    chore: optiond

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,15 @@
 "use strict";
 
 import * as functions from "firebase-functions";
-import {dialogflow} from "./dialogflow";
+import { dialogflow } from "./dialogflow";
 
-const runtimeOpts = {
-  timeoutSeconds: 8,
-  memory: "1GB" as const,
-  minInstances: 1,
-};
-const region = "asia-northeast1";
+// const runtimeOpts = {
+//   timeoutSeconds: 8,
+//   memory: "1GB" as const,
+//   minInstances: 1,
+// };
+// const region = "asia-northeast1";
 
-exports.dialogflowFirebaseFulfillment = functions
-    .region(region)
-    .runWith(runtimeOpts)
-    .https.onRequest(dialogflow);
+exports.dialogflowFirebaseFulfillment = functions// .runWith(runtimeOpts) // .region(region)
+.https
+  .onRequest(dialogflow);
