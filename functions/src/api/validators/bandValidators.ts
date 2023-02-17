6648e25a3136961d9db697aef879730fc48e84c9
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2023-02-17 21:49:29 +0700

    feat: fields validation

--- functions/src/api/validators/bandValidators.ts
+++ functions/src/api/validators/bandValidators.ts
@@ -3,8 +3,27 @@ import {body} from "express-validator";
 export const getBandsValidators = [body("userId").notEmpty()];
 
 export const createBandValidators = [
-  body("userId").notEmpty(),
   body("bandName").notEmpty(),
-  body("firstPromotedSong").notEmpty().bail().isString(),
-  body("secondPromotedSong").notEmpty().bail().isString(),
+  body("firstPromotedSong").optional().isString(),
+  body("secondPromotedSong").optional().isString(),
+  body("userId").notEmpty(),
+
+  body("socialMedia").optional(),
+  body("socialMedia.facebook").optional().isString(),
+  body("socialMedia.instragram").optional().isString(),
+  body("socialMedia.tiktok").optional().isString(),
+  body("socialMedia.website").optional().isString(),
+
+  body("streamingPlatform").optional(),
+  body("streamingPlatform.spotify").optional().isString(),
+  body("streamingPlatform.youtube").optional().isString(),
+  body("streamingPlatform.apple_music").optional().isString(),
+
+  body("lineMelody").optional().isString(),
+  body("songRequest").optional().isBoolean(),
+  body("description").optional().isString(),
+
+  body("lineBeacon").optional(),
+  body("lineBeacon.*.hardware_id").isString(),
+  body("lineBeacon.*.passcode").isString(),
 ];
