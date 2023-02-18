3585e06dc224ee9358b9e1d409a4854ebec6f743
Napatchol Thaipanich <patorseing@gmail.com>
2023-02-18 09:26:13 +0700

    chore: fix lint on healthcheck

--- functions/src/api/routes/healthcheck.ts
+++ functions/src/api/routes/healthcheck.ts
@@ -1,9 +1,10 @@
 import * as express from "express";
 
+/* eslint new-cap: "warn"*/
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  res.status(200).json({ data: "ok" });
+  res.status(200).json({data: "ok"});
 });
 
 export default router;
