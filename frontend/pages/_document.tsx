9105a4eee470d137789c605d5b5971d8998e39ad
Napatchol Thaipanich <patorseing@gmail.com>
2023-02-18 09:42:09 +0700

    chore: remove unnecessary

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,7 +9,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
           rel="stylesheet"
         />
-        <script src="https://www.google.com/recaptcha/api.js" />
       </Head>
       <body>
         <Main />
