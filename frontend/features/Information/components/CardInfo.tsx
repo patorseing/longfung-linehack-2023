0a03a9d7aa09f0eedb3645d0e36b32233ec7e789
WasitaL <wasita.pn@gmail.com>
2023-02-19 23:34:32 +0700

    feat: creat information page

---
+++ frontend/features/Information/components/CardInfo.tsx
@@ -0,0 +1,53 @@
+import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
+type Props = {
+  img: string;
+  name: string;
+};
+export const CardInfo = ({ img, name }: Props) => {
+  return (
+    <VStack
+      sx={{
+        w: { base: "250px", md: "220px" },
+        h: "250px",
+        bg: "white",
+        borderRadius: "10px",
+        boxShadow:
+          "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
+        cursor: "pointer",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          w: "inherit",
+          h: "180px",
+          overflow: "hidden",
+        }}
+      >
+        <Image
+          src="/images/event-info.png"
+          sx={{
+            w: "inherit",
+            borderTopRadius: "10px",
+          }}
+        />
+      </Box>
+      <Flex
+        sx={{
+          alignItems: "center",
+          h: { base: `calc(250px - 180px)`, md: `calc(250px - 180px)` },
+          mt: "0 !important",
+        }}
+      >
+        <Text
+          sx={{
+            fontSize: { base: "16px", md: "20px" },
+            fontWeight: "bold",
+          }}
+        >
+          {name}
+        </Text>
+      </Flex>
+    </VStack>
+  );
+};
