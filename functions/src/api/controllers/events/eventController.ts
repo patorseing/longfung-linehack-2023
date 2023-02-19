f0e37fe7657df755613c8accf39bbc13868624ac
Supermalonz <kavekhunsri@gmail.com>
2023-02-19 20:17:47 +0700

    feat: udpated event controller

--- functions/src/api/controllers/events/eventController.ts
+++ functions/src/api/controllers/events/eventController.ts
@@ -1,3 +1,79 @@
 import {Request, Response} from "express";
-import {validationResult} from "express-validator";
+// import {validationResult} from "express-validator";
 import * as functions from 'firebase-functions'
+
+import {compact} from "../../utils/payload";
+import {firestore} from "../../../firebase";
+import {Event} from "../../dto/event";
+import {fileUploader} from "../../utils/fileUploader";
+
+
+export const getEvents = async (req: Request, res: Response) => {
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()){
+  //   return res.status(400).json({errors: errors["errors"]});
+  // }
+
+  const eventList: FirebaseFirestore.DocumentData[] = [];
+
+  await firestore
+      .collection("Event")
+      .where("userId", "==", req.body["userId"])
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          eventList.push(doc.data());
+        });
+      });
+
+  return res.status(200).json({data: eventList});
+};
+
+export const createEvent = async (req: Request, res: Response) => {
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json({errors: errors["errors"]});
+  // }
+
+  try {
+    const event: Event = {
+      eventName: req.body.eventName,
+      eventImage: req.body.eventImage || null,
+      ticketType: req.body.ticketType || {},
+      available_seat: req.body.available_seat || null,
+      age_limitation: req.body.age_limitation || null,
+      eventDate: req.body.eventDate,
+      eventStartTime: req.body.eventStartTime,
+      eventEndTime: req.body.eventEndTime,
+      eventLocation: req.body.eventLocation || {},
+      interestedPerson: req.body.interestedPerson || [],
+      socialMedia: req.body.socialMedia || {},
+      lineBeacon: req.body.lineBeacon || [],
+    };
+
+    const bucketName = functions.config().uploader.bucket_name;
+
+
+    if (req.body.eventImage !== undefined) {
+      const imageUrl = await fileUploader(bucketName, req.body.eventImage);
+
+      event.eventImage = imageUrl;
+    }
+
+    event.lineBeacon?.forEach(async (el) => {
+      await firestore.collection("LineBeacon").doc(el.hardwareId).set({
+        hardwareId: el.hardwareId,
+        eventName: event.eventName,
+      });
+    });
+
+    const newEvent = await firestore
+        .collection("Event")
+        .doc(event.eventName)
+        .set(compact(event))
+
+    return res.status(201).send({data: newEvent});
+    } catch (err) {
+      return res.status(422).send(err);
+  }
+};
--- functions/src/api/validators/eventValidators.ts
+++ functions/src/api/validators/eventValidators.ts
@@ -1,39 +1,39 @@
-import {body} from "express-validator";
+// import {body} from "express-validator";
 
-export const getEventsValidators = [body("userId").notEmpty()];
+// export const getEventsValidators = [body("userId").notEmpty()];
 
-export const createEventValidators = [
-  body("eventName").notEmpty(),
-  body("eventDate").notEmpty(),
-  body("eventTime").notEmpty(),
+// export const createEventValidators = [
+//   body("eventName").notEmpty(),
+//   body("eventDate").notEmpty(),
+//   body("eventTime").notEmpty(),
 
-  body("socialMedia").optional(),
-  body("socialMedia.facebook").optional().isString(),
-  body("socialMedia.instagram").optional().isString(),
-  body("socialMedia.website").optional().isString(),
+//   body("socialMedia").optional(),
+//   body("socialMedia.facebook").optional().isString(),
+//   body("socialMedia.instagram").optional().isString(),
+//   body("socialMedia.website").optional().isString(),
 
-  body("location").isString().notEmpty(),
-  body("googleMapUrl").optional().isString(),
+//   body("location").isString().notEmpty(),
+//   body("googleMapUrl").optional().isString(),
 
-  body("availableSeat").isInteger().notEmpty(),
-  body("ageLimit").isInteger().optional(),
+//   body("availableSeat").isInteger().notEmpty(),
+//   body("ageLimit").isInteger().optional(),
 
-  body("ticketType").notEmpty(),
-  body("ticketType.*.free").isBoolean().optional(),
-  body("ticketType.*.price").isInteger().optional(),
-  body("alcoholPermission").notEmpty(),
-  body("alcoholPermission.*.alcohol").isBoolean().optional(),
-  body("alcoholPermission.*.noAlcohol").isBoolean().optional(),
+//   body("ticketType").notEmpty(),
+//   body("ticketType.*.free").isBoolean().optional(),
+//   body("ticketType.*.price").isInteger().optional(),
+//   body("alcoholPermission").notEmpty(),
+//   body("alcoholPermission.*.alcohol").isBoolean().optional(),
+//   body("alcoholPermission.*.noAlcohol").isBoolean().optional(),
 
-  body("songRequest").optional().isBoolean(),
-  body("description").optional().isString(),
+//   body("songRequest").optional().isBoolean(),
+//   body("description").optional().isString(),
 
-  body("eventImage").optional().notEmpty(),
-  body("lineBeacon").optional(),
-  body("lineBeacon.*.hardwareId").isString(),
-  body("lineBeacon.*.passcode").isString(),
+//   body("eventImage").optional().notEmpty(),
+//   body("lineBeacon").optional(),
+//   body("lineBeacon.*.hardwareId").isString(),
+//   body("lineBeacon.*.passcode").isString(),
 
-  body("lineUpSchedule").notEmpty(),
-  body("lineUpSchedule.*.time").notEmpty(),
-  body("lineUpSchedule.*.band").notEmpty()
-];
+//   body("lineUpSchedule").notEmpty(),
+//   body("lineUpSchedule.*.time").notEmpty(),
+//   body("lineUpSchedule.*.band").notEmpty()
+// ];
