630a1741a2b006bbbf3096744c57ddc586394545
Napatchol Thaipanich <patorseing@gmail.com>
2023-02-19 12:33:34 +0700

    chore: clear dialogflow

--- functions/src/dialogflow/func/inputNumber.ts
+++
@@ -1,9 +0,0 @@
-import {WebhookClient} from "dialogflow-fulfillment";
-
-export const inputNumber = (agent: WebhookClient) => {
-  const num = agent.parameters.num;
-
-  console.log(num);
-
-  agent.add(`You said: ${num}`);
-};
