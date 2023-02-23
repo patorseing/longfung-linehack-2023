7030f9325643c9c5da6ff7128f921d90ff25219b
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2023-02-23 20:22:42 +0700

    chore: remove log

--- functions/src/api/controllers/bands/bandController.ts
+++ functions/src/api/controllers/bands/bandController.ts
@@ -79,11 +79,7 @@ export const createBand = async (req: Request, res: Response) => {
 
           const bucketName = functions.config().uploader.bucket_name;
 
-          console.log(bucketName);
-
           const bandImage = files.bandImage;
-
-          console.log(bandImage);
 
           if (bandImage !== undefined) {
             const imageUrl = await fileUploader(bucketName, bandImage.path);
